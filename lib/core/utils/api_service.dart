3f585c04883894ab2301640167bfe7d0f744f3ca
alimagdy <alimagdy973@gmail>
2026-03-31 21:38:14 -0700

    fix : server.js

--- lib/core/utils/api_service.dart
+++ lib/core/utils/api_service.dart
@@ -3,7 +3,7 @@ import 'package:dio/dio.dart';
 class ApiService {
   final Dio _dio = Dio(
     BaseOptions(
-      baseUrl: "https://robt-unkinglike-insipidly.ngrok-free.dev", 
+      baseUrl: "https://getawaywithstripe-production.up.railway.app", 
       connectTimeout: const Duration(seconds: 30),
       receiveTimeout: const Duration(seconds: 30),
       headers: {
